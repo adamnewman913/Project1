a8a31e0b8e0d3007fbb4b3602172b5abb3d90a78
Jenny Vossman <jenvoss17@gmail.com>
2019-07-27 07:20:27 -0500

    remove unwanted merge remnants | update sidenav | display map

--- home-assets/SingleDay.js
+++ home-assets/SingleDay.js
@@ -7,41 +7,41 @@
  
  */
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        var pos = [
-         position.coords.latitude,
-         position.coords.longitude
+  navigator.geolocation.getCurrentPosition(function (position) {
+    var pos = [
+      position.coords.latitude,
+      position.coords.longitude
     ];
     var WAPIKey = "dda695a70c57593ba9c2432172634826";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-   "lat="+ pos[0] +"&lon="+ pos[1] +"&appid=" + WAPIKey;
-   
-   $.ajax({
-     url: queryURL,
-     method: "GET"
-   })
-     // We store all of the retrieved data inside of an object called "response"
-     .then(function(response) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+      "lat=" + pos[0] + "&lon=" + pos[1] + "&appid=" + WAPIKey;
 
-       // Log the queryURL
-       console.log(queryURL);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // We store all of the retrieved data inside of an object called "response"
+      .then(function (response) {
 
-       // Log the resulting object
-       console.log(response);
-       var tempF = parseInt((response.main.temp - 273.15) * 1.80 + 32);
-       // Transfer content to HTML
-       $("#map").html("<h3>Today's Weather   " + tempF + "(F)</h3>");
-       $("#map").append("Wind Speed: " + response.wind.speed + "<br>");
-       $("#map").append("Humidity: " + response.main.humidity);
-       $("#homepageText").html("<h1>Today's Weather Forecast is " + tempF + "(F) with " + response.weather[0].main + " Skies with " + response.wind.speed + " MPH Winds")
-//NEED TO CONVERT KELVIN TO F
-       // Log the data in the console as well
-     });
+        // Log the queryURL
+        console.log(queryURL);
 
-    }, function () {
-        handleLocationError(true, infoWindow, map.getCenter());
-    });
+        // Log the resulting object
+        console.log(response);
+        var tempF = parseInt((response.main.temp - 273.15) * 1.80 + 32);
+        // Transfer content to HTML
+        $("#today").html("<h3>Today's Weather   " + tempF + "(F)</h3>");
+        $("#today").append("Wind Speed: " + response.wind.speed + "<br>");
+        $("#today").append("Humidity: " + response.main.humidity);
+        $("#homepageText").html("<h1>Today's Weather Forecast is " + tempF + "(F) with " + response.weather[0].main + " Skies with " + response.wind.speed + " MPH Winds")
+        //NEED TO CONVERT KELVIN TO F
+        // Log the data in the console as well
+      });
+
+  }, function () {
+    handleLocationError(true, infoWindow, map.getCenter());
+  });
 } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
+  // Browser doesn't support Geolocation
+  handleLocationError(false, infoWindow, map.getCenter());
 }
--- nearby/nearby.js
+++ nearby/nearby.js
@@ -11,9 +11,9 @@ placeInput = $("#inputPlace").val().trim();
 // onto global map variable through some
 
 
-function initAutocomplete() {
+function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -33.8688, lng: 151.2195 },
+        center: chicago,
         zoom: 13,
         mapTypeId: 'roadmap'
     });
